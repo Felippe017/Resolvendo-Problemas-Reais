cfbd40b8c4a820648e327192f961bef588d81515
Felippe017 <felippecorrea2010@gmail.com>
2023-09-07 08:45:28 -0300

    feat/desafio-1: Adiciona elemento Fragment como elemento pai dos dois componentes span. Adiciona comentários explicando o problema e como foi resolvido.

--- src/Desafio1/index.tsx
+++ src/Desafio1/index.tsx
@@ -3,12 +3,23 @@
   de errado e também a solução que você encontrou.
 
   Resposta / comentários sobre a sua solução:
+  
+  O componente abaixo apresentava um problema de sintaxe. Dentro do retorno do
+  componente, eram retornados dois elementos "span" diretamente um após o outro,
+  mas o JSX exige que aja um único elemento pai que englobe todos os outros
+  elementos filhos, sendo esse um requisito do JSX para que o código seja compilado
+  corretamente.
 
+  A solução foi então envolver os elementos "span" em um único elemento pai, nesse 
+  caso, envolvi com um Fragment. Assim, o problema de sintaxe foi resolvido e permitiu
+  que o JSX fosse compilado corretamente.
 */
 
 export const Desafio1 = () => {
   return (
+    <>
       <span>item 1</span>
       <span>item 2</span>
+    </>
   )
 }
