e8ea0f56954b7094b971df63f960e42044c688ca
Felippe017 <felippecorrea2010@gmail.com>
2023-09-07 13:14:30 -0300

    feat/desafio-4: Resolvendo problemas no componente Desafio4 (listagem não parava de atualizar e total só mostrava o valor real sem considerar o valor da contagem 1) e refatorando-o

--- src/Desafio4/index.tsx
+++ src/Desafio4/index.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
 /* TODO
   Existe neste componente alguns problemas.
@@ -12,30 +12,45 @@ import React, {useEffect, useState} from 'react';
   - Não usar mais de um useEffect.
   - O cálculo do total deverá ser feito no useEffect.
   - As alterações do estados da lista e total deverá ocorrer no useEffect.
+
+   Para resolver o problema em que a listagem atualiza descontroladamente e
+   exponencialmente, tirei o list do array de dependências do React. Adicionei
+   o state "count1" na lista de dependências do useEffect para considerá-lo
+   também na contagem. Tudo isso obedencendo os critérios pré-estabelecidos.
+   Além disso, o componente foi tipado para boa manutenção do código e legibilidade.
 */
 
-export const Desafio4 = () => {
-  const [count1, setCount1] = useState(0)
-  const [count2, setCount2] = useState(0)
-  const [count3, setCount3] = useState(0)
+export const Desafio4: React.FC = () => {
+  const [count1, setCount1] = useState<number>(0)
+  const [count2, setCount2] = useState<number>(0)
+  const [count3, setCount3] = useState<number>(0)
   const [list, setList] = useState<number[]>([])
-  const [total, setTotal] = useState(0)
+  const [total, setTotal] = useState<number>(0)
+  
+  useEffect(() => {
+    const totalCount: number = count1 + count2 + count3;
+    setTotal(totalCount);
+    setList([...list, totalCount]);
+  }, [count1, count2, count3]);
 
-  useEffect(()=>{
-    const totalCount = count1 + count2 + count3;
-    setTotal(totalCount)
-    setList([...list, totalCount])
-  },[count3, count2, list])
-
-  const addCount1 = ()=>setCount1(count1 + 1)
-  const addCount2 = ()=>setCount2(count2 + 1)
-  const addCount3 = ()=>setCount3(count3 + 1)
+  const addCount1 = () => setCount1(count1 + 1)
+  const addCount2 = () => setCount2(count2 + 1)
+  const addCount3 = () => setCount3(count3 + 1)
 
   return (
     <>
-      <button onClick={addCount1} data-testid="btn1">adiciona + 1 na contagem 1</button><br/>
-      <button onClick={addCount2} data-testid="btn2">adiciona + 1 na contagem 2</button><br/>
-      <button onClick={addCount3} data-testid="btn3">adiciona + 1 na contagem 3</button><br/>
+      <button onClick={ addCount1 } data-testid="btn1">
+        adiciona + 1 na contagem 1
+      </button>
+      <br/>
+      <button onClick={addCount2} data-testid="btn2">
+        adiciona + 1 na contagem 2
+      </button>
+      <br/>
+      <button onClick={addCount3} data-testid="btn3">
+        adiciona + 1 na contagem 3
+      </button>
+      <br/>
       <p data-testid="v1">v1: {count1}</p>
       <p data-testid="v2">v2: {count2}</p>
       <p data-testid="v3">v3: {count3}</p>
